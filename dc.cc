47b657fb8d67beb88867fb249009415cd270c5ad
acdzh <1069436872@qq.com>
2018-12-07 21:42:03 +0800

    注释

--- dc.cc
+++ dc.cc
@@ -5,7 +5,7 @@
 #include "ns3/applications-module.h"
 #include "ns3/ipv4-global-routing-helper.h"
 
-//#include "ns3/netanim-module.h"//默认不输出xml
+//#include "ns3/netanim-module.h" //Default does not output xml
 
 
 using namespace ns3;
@@ -17,92 +17,98 @@ main(int argc, char *argv[])
 {
 
 	uint32_t nCsma = 3;
-
+	
+	//output log
 	LogComponentEnable("PacketSink", LOG_LEVEL_INFO);
 	LogComponentEnable("OnOffApplication", LOG_LEVEL_INFO);
 
 	nCsma = nCsma == 0 ? 1 : nCsma;
 
-	//创建底层的4个小网络
+	
+	
+	//Create the bottom 4 small networks
 	NodeContainer csmaNodes1;
-	csmaNodes1.Create(3);
+	csmaNodes1.Create(3); //create t1 -- (n1, n2)
 	NodeContainer csmaNodes2;
-	csmaNodes2.Create(3);
+	csmaNodes2.Create(3); //create t2 -- (n3, n4)
 	NodeContainer csmaNodes3;
-	csmaNodes3.Create(3);
+	csmaNodes3.Create(3); //create t3 -- (n5, n6)
 	NodeContainer csmaNodes4;
-	csmaNodes4.Create(3);
+	csmaNodes4.Create(3); //creat t4 -- (n7, n8)
 
 
-	//创建上层的csma中左侧网络
+	//Create the lefter network in the upper csma //a1
 	NodeContainer csmaNodes5;
 	csmaNodes5.Create(1);
-	csmaNodes5.Add(csmaNodes1.Get(0));
-	csmaNodes5.Add(csmaNodes2.Get(0));
-	//创建上层的csma中右侧网络  
+	csmaNodes5.Add(csmaNodes1.Get(0)); //link t1->a1
+	csmaNodes5.Add(csmaNodes2.Get(0)); //link t2->a1
+	
+	//Create the righter network in the upper csma  //a2
 	NodeContainer csmaNodes6;
 	csmaNodes6.Create(1);
-	csmaNodes6.Add(csmaNodes3.Get(0));
-	csmaNodes6.Add(csmaNodes4.Get(0));
-	//创建总的csma大网络  
+	csmaNodes6.Add(csmaNodes3.Get(0)); //link t3->a2
+	csmaNodes6.Add(csmaNodes4.Get(0)); //link t4->a2
+	
+	//Create a total csma big network //c1 
 	NodeContainer csmaNodes7;
 	csmaNodes7.Create(1);
-	csmaNodes7.Add(csmaNodes5.Get(0));
-	csmaNodes7.Add(csmaNodes6.Get(0));
+	csmaNodes7.Add(csmaNodes5.Get(0)); //link a1->c1
+	csmaNodes7.Add(csmaNodes6.Get(0)); //link a2->c1
 
-	//CMSA1网络拓扑
+	
+	
+	//CSMA1 network topology // topology: t1 -- (n1, n2)
 	CsmaHelper csma1;
 	csma1.SetChannelAttribute("DataRate", StringValue("1.0Mbps"));
 	csma1.SetChannelAttribute("Delay", TimeValue(NanoSeconds(500)));
-
 	NetDeviceContainer csmaDevices1;
 	csmaDevices1 = csma1.Install(csmaNodes1);
-	//CMSA2网络拓扑
+	
+	//CSMA2 network topology // topology: t2 -- (n3, n4)
 	CsmaHelper csma2;
 	csma2.SetChannelAttribute("DataRate", StringValue("1.0Mbps"));
 	csma2.SetChannelAttribute("Delay", TimeValue(NanoSeconds(500)));
-
 	NetDeviceContainer csmaDevices2;
 	csmaDevices2 = csma2.Install(csmaNodes2);
-	//CMSA3网络拓扑
+	
+	//CSMAA3 network topology // topology: t3 -- (n5, n6)
 	CsmaHelper csma3;
 	csma3.SetChannelAttribute("DataRate", StringValue("1.0Mbps"));
 	csma3.SetChannelAttribute("Delay", TimeValue(NanoSeconds(500)));
-
 	NetDeviceContainer csmaDevices3;
 	csmaDevices3 = csma3.Install(csmaNodes3);
-	//CMSA4网络拓扑
+	
+	//CSMA4 network topology // topology: t4 -- (n7, n8)
 	CsmaHelper csma4;
 	csma4.SetChannelAttribute("DataRate", StringValue("1.0Mbps"));
 	csma4.SetChannelAttribute("Delay", TimeValue(NanoSeconds(500)));
-
 	NetDeviceContainer csmaDevices4;
 	csmaDevices4 = csma4.Install(csmaNodes4);
-	//CMSA5网络拓扑
+	
+	//CSMA5 network topology // topology: a1 -- (t1, t2)
 	CsmaHelper csma5;
 	csma5.SetChannelAttribute("DataRate", StringValue("1.0Mbps"));
 	csma5.SetChannelAttribute("Delay", TimeValue(NanoSeconds(500)));
-
 	NetDeviceContainer csmaDevices5;
 	csmaDevices5 = csma5.Install(csmaNodes5);
-	//CMSA6网络拓扑
+	
+	//CSMA6 network topology // topology: a2 -- (t3, t4)
 	CsmaHelper csma6;
 	csma6.SetChannelAttribute("DataRate", StringValue("1.0Mbps"));
 	csma6.SetChannelAttribute("Delay", TimeValue(NanoSeconds(500)));
-
 	NetDeviceContainer csmaDevices6;
 	csmaDevices6 = csma6.Install(csmaNodes6);
-	//CMSA7网络拓扑
+	
+	//CSMA7 network topology // topology: c1 -- (a1, a2)
 	CsmaHelper csma7;
 	csma7.SetChannelAttribute("DataRate", StringValue("1.5Mbps"));
 	csma7.SetChannelAttribute("Delay", TimeValue(NanoSeconds(500)));
-
 	NetDeviceContainer csmaDevices7;
 	csmaDevices7 = csma7.Install(csmaNodes7);
 
 
 
-	//安装协议栈（每个node只有一个stack）
+	//Installation protocol stack (every node just has one stack)
 	InternetStackHelper stack;
 	stack.Install(csmaNodes1);
 	stack.Install(csmaNodes2);
@@ -112,85 +118,109 @@ main(int argc, char *argv[])
 	stack.Install(csmaNodes6.Get(0));
 	stack.Install(csmaNodes7.Get(0));
 
-	//在p2p网络上分配地址  node0：10.1.1.1；node1： 10.1.1.2
+	
+	
+	//Assign addresses on the p2p network
+	//node0: 10.1.1.1
+	//node1:  10.1.1.2
 	Ipv4AddressHelper address;
 
 
-	//在CSMA1网络上分配地址，相当于t1下端口IP为10.0.1.1，其余主机为n1 IP为10.0.1.2
-	//n2主机IP为10.0.1.3
+	
+	//Assign addresses on the CSMA1 network: 
+		//t1-low-port ip: 10.0.1.1
+		//n1 ip: 10.0.1.2;	n2 ip: 10.0.1.3
 	address.SetBase("10.0.1.0", "255.255.255.0");
 	Ipv4InterfaceContainer csmaInterfaces1;
 	csmaInterfaces1 = address.Assign(csmaDevices1);
-	//在CSMA2网络上分配地址，同CSMA1网络IP分配
+	
+	//Assign addresses on the CSMA2 network: 
+		//t2-low-port ip: 10.0.2.1
+		//n3 ip: 10.0.2.2;	n4 ip: 10.0.2.3
 	address.SetBase("10.0.2.0", "255.255.255.0");
 	Ipv4InterfaceContainer csmaInterfaces2;
 	csmaInterfaces2 = address.Assign(csmaDevices2);
-	//在CSMA3网络上分配地址，同CSMA1网络IP分配
+	
+	//Assign addresses on the CSMA3 network: 
+		//t3-low-port ip: 10.0.3.1
+		//n5 ip: 10.0.3.2;	n6 ip: 10.0.3.3
 	address.SetBase("10.0.3.0", "255.255.255.0");
 	Ipv4InterfaceContainer csmaInterfaces3;
 	csmaInterfaces3 = address.Assign(csmaDevices3);
-	//在CSMA4网络上分配地址，同CSMA1网络IP分配
+	
+	//Assign addresses on the CSMA4 network: 
+		//t4-low-port ip: 10.0.4.1
+		//n7 ip: 10.0.4.2;	n8 ip: 10.0.4.3
 	address.SetBase("10.0.4.0", "255.255.255.0");
 	Ipv4InterfaceContainer csmaInterfaces4;
 	csmaInterfaces4 = address.Assign(csmaDevices4);
-	//在CSMA5网络上分配地址，相当于a1下端口IP为10.1.1.1，其余交换机为t1上端口 IP为10.1.1.2
-	//t2上端口IP为10.1.1.3
+	
+	//Assign addresses on the CSMA5 network: 
+		//a1-low-port ip: 10.1.1.1
+		//t1-upper-port ip: 10.1.1.2;	t2-upper-port ip: 10.1.1.3
 	address.SetBase("10.1.1.0", "255.255.255.0");
 	Ipv4InterfaceContainer csmaInterfaces5;
 	csmaInterfaces5 = address.Assign(csmaDevices5);
-	//在CSMA6网络上分配地址，同CSMA5中网络IP分配
+	
+	//Assign addresses on the CSMA6 network: 
+		//a2-low-port ip: 10.2.1.1
+		//t3-upper-port ip: 10.2.1.2;	t4-upper-port ip: 10.2.1.3
 	address.SetBase("10.2.1.0", "255.255.255.0");
 	Ipv4InterfaceContainer csmaInterfaces6;
 	csmaInterfaces6 = address.Assign(csmaDevices6);
-	//在CSMA7网络上分配地址，相当于c1端口IP为192.168.1.1，其余交换机为a1上端口 IP为192.168.1.2
-	//t2上端口IP为192.168.1.3
+	
+	//Assign addresses on the CSMA7 network: 
+		//c1 ip: 192.168.1.1
+		//a1-upper-port ip: 192.168.1.2;	a2-upper-port ip: 192.168.1.3
 	address.SetBase("192.168.1.0", "255.255.255.0");
 	Ipv4InterfaceContainer csmaInterfaces7;
 	csmaInterfaces7 = address.Assign(csmaDevices7);
 
-	//host h1 server
+	
+	
+	//host n1 server TcpSocketApp
 	ApplicationContainer sinkApp1;
 	PacketSinkHelper packetSinkHelper1("ns3::TcpSocketFactory", InetSocketAddress(csmaInterfaces1.GetAddress(1), 5000));
 	sinkApp1 = packetSinkHelper1.Install(csmaNodes1.Get(1));
 	sinkApp1.Start(Seconds(1.0));
 	sinkApp1.Stop(Seconds(60.0));
-	//host h2 server 
+	//host n2 server TcpSocketApp
 	ApplicationContainer sinkApp2;
 	PacketSinkHelper packetSinkHelper2("ns3::TcpSocketFactory", InetSocketAddress(csmaInterfaces1.GetAddress(2), 5000));
 	sinkApp2 = packetSinkHelper2.Install(csmaNodes1.Get(2));
 	sinkApp2.Start(Seconds(1.0));
 	sinkApp2.Stop(Seconds(60.0));
-	//host h3 server
+	//host n3 server TcpSocketApp
 	ApplicationContainer sinkApp3;
 	PacketSinkHelper packetSinkHelper3("ns3::TcpSocketFactory", InetSocketAddress(csmaInterfaces2.GetAddress(1), 5000));
 	sinkApp3 = packetSinkHelper3.Install(csmaNodes2.Get(1));
 	sinkApp3.Start(Seconds(1.0));
 	sinkApp3.Stop(Seconds(60.0));
-	//host h4 server
+	//host n4 server TcpSocketApp
 	ApplicationContainer sinkApp4;
 	PacketSinkHelper packetSinkHelper4("ns3::TcpSocketFactory", InetSocketAddress(csmaInterfaces2.GetAddress(2), 5000));
 	sinkApp4 = packetSinkHelper4.Install(csmaNodes2.Get(2));
 	sinkApp4.Start(Seconds(1.0));
 	sinkApp4.Stop(Seconds(60.0));
-	//host h5 server
+	//host n5 server TcpSocketApp
 	ApplicationContainer sinkApp5;
 	PacketSinkHelper packetSinkHelper5("ns3::TcpSocketFactory", InetSocketAddress(csmaInterfaces3.GetAddress(1), 5000));
 	sinkApp5 = packetSinkHelper5.Install(csmaNodes3.Get(1));
 	sinkApp5.Start(Seconds(1.0));
 	sinkApp5.Stop(Seconds(60.0));
-	//host h6 server
+	//host n6 server TcpSocketApp
 	ApplicationContainer sinkApp6;
 	PacketSinkHelper packetSinkHelper6("ns3::TcpSocketFactory", InetSocketAddress(csmaInterfaces3.GetAddress(2), 5000));
 	sinkApp6 = packetSinkHelper6.Install(csmaNodes3.Get(2));
 	sinkApp6.Start(Seconds(1.0));
 	sinkApp6.Stop(Seconds(60.0));
-	//host h7 server
+	//host n7 server TcpSocketApp
 	ApplicationContainer sinkApp7;
 	PacketSinkHelper packetSinkHelper7("ns3::TcpSocketFactory", InetSocketAddress(csmaInterfaces4.GetAddress(1), 5000));
 	sinkApp7 = packetSinkHelper7.Install(csmaNodes4.Get(1));
 	sinkApp7.Start(Seconds(1.0));
 	sinkApp7.Stop(Seconds(60.0));
-	//host h8 server
+	//host n8 server TcpSocketApp
 	ApplicationContainer sinkApp8;
 	PacketSinkHelper packetSinkHelper8("ns3::TcpSocketFactory", InetSocketAddress(csmaInterfaces4.GetAddress(2), 5000));
 	sinkApp8 = packetSinkHelper8.Install(csmaNodes4.Get(2));
@@ -198,9 +228,10 @@ main(int argc, char *argv[])
 	sinkApp8.Stop(Seconds(60.0));
 
 
-	//在节点n1上建立多个OnOffApplication，产生多路流量,用于接收其他客户机发来的数据包
-	//On/Off周期性地产生流量，这里将On周期设置为50秒，Off周期设置为0
-	  //host   h1--->h2
+	//Simulated the situation in which some hosts send and receive data to and from each other
+	//Create multiple OnOffApplications on node n1 to generate multiple traffic for receiving packets from other clients.
+	//On/Off periodically generates traffic, where the On cycle is set to 50 seconds and the Off cycle is set to 0.
+		//host   n1--->n2
 	ApplicationContainer clientApp1;
 	OnOffHelper client1("ns3::TcpSocketFactory", InetSocketAddress(csmaInterfaces1.GetAddress(2), 5000));
 	client1.SetAttribute("OnTime", StringValue("ns3::ConstantRandomVariable[Constant=50]"));
@@ -210,7 +241,7 @@ main(int argc, char *argv[])
 	clientApp1 = client1.Install(csmaNodes1.Get(1));
 	clientApp1.Start(Seconds(2));
 	clientApp1.Stop(Seconds(3));
-	//host   h1--->h3
+		//host   n1--->n3
 	ApplicationContainer clientApp2;
 	OnOffHelper client2("ns3::TcpSocketFactory", InetSocketAddress(csmaInterfaces2.GetAddress(1), 5000));
 	client2.SetAttribute("OnTime", StringValue("ns3::ConstantRandomVariable[Constant=50]"));
@@ -220,7 +251,7 @@ main(int argc, char *argv[])
 	clientApp2 = client2.Install(csmaNodes1.Get(1));
 	clientApp2.Start(Seconds(2));
 	clientApp2.Stop(Seconds(3));
-	//host  h5--->h6
+		//host  n5--->n6
 	ApplicationContainer clientApp3;
 	OnOffHelper client3("ns3::TcpSocketFactory", InetSocketAddress(csmaInterfaces3.GetAddress(2), 5000));
 	client3.SetAttribute("OnTime", StringValue("ns3::ConstantRandomVariable[Constant=50]"));
@@ -230,7 +261,7 @@ main(int argc, char *argv[])
 	clientApp3 = client3.Install(csmaNodes3.Get(1));
 	clientApp3.Start(Seconds(2));
 	clientApp3.Stop(Seconds(3));
-	//host  h5--->h7
+		//host  n5--->n7
 	ApplicationContainer clientApp4;
 	OnOffHelper client4("ns3::TcpSocketFactory", InetSocketAddress(csmaInterfaces4.GetAddress(1), 5000));
 	client4.SetAttribute("OnTime", StringValue("ns3::ConstantRandomVariable[Constant=50]"));
@@ -240,7 +271,7 @@ main(int argc, char *argv[])
 	clientApp4 = client4.Install(csmaNodes3.Get(1));
 	clientApp4.Start(Seconds(2));
 	clientApp4.Stop(Seconds(3));
-	//host  h1--->h5
+		//host  n1--->n5
 	ApplicationContainer clientApp5;
 	OnOffHelper client5("ns3::TcpSocketFactory", InetSocketAddress(csmaInterfaces3.GetAddress(1), 5000));
 	client5.SetAttribute("OnTime", StringValue("ns3::ConstantRandomVariable[Constant=50]"));
@@ -250,7 +281,7 @@ main(int argc, char *argv[])
 	clientApp5 = client5.Install(csmaNodes1.Get(1));
 	clientApp5.Start(Seconds(2));
 	clientApp5.Stop(Seconds(3));
-	//host  h6--->h2
+		//host  n6--->n2
 	ApplicationContainer clientApp6;
 	OnOffHelper client6("ns3::TcpSocketFactory", InetSocketAddress(csmaInterfaces1.GetAddress(2), 5000));
 	client6.SetAttribute("OnTime", StringValue("ns3::ConstantRandomVariable[Constant=50]"));
@@ -260,7 +291,7 @@ main(int argc, char *argv[])
 	clientApp6 = client6.Install(csmaNodes3.Get(2));
 	clientApp6.Start(Seconds(2));
 	clientApp6.Stop(Seconds(3));
-	//host  h3--->h7
+		//host  n3--->n7
 	ApplicationContainer clientApp7;
 	OnOffHelper client7("ns3::TcpSocketFactory", InetSocketAddress(csmaInterfaces4.GetAddress(1), 5000));
 	client7.SetAttribute("OnTime", StringValue("ns3::ConstantRandomVariable[Constant=50]"));
@@ -270,7 +301,7 @@ main(int argc, char *argv[])
 	clientApp7 = client7.Install(csmaNodes2.Get(1));
 	clientApp7.Start(Seconds(2));
 	clientApp7.Stop(Seconds(3));
-	//host  h8--->h4
+		//host  n8--->n4
 	ApplicationContainer clientApp8;
 	OnOffHelper client8("ns3::TcpSocketFactory", InetSocketAddress(csmaInterfaces2.GetAddress(2), 5000));
 	client8.SetAttribute("OnTime", StringValue("ns3::ConstantRandomVariable[Constant=50]"));
@@ -280,7 +311,7 @@ main(int argc, char *argv[])
 	clientApp8 = client8.Install(csmaNodes4.Get(2));
 	clientApp8.Start(Seconds(2));
 	clientApp8.Stop(Seconds(3));
-	//host  h2--->h8
+		//host  n2--->n8
 	ApplicationContainer clientApp9;
 	OnOffHelper client9("ns3::TcpSocketFactory", InetSocketAddress(csmaInterfaces4.GetAddress(2), 5000));
 	client9.SetAttribute("OnTime", StringValue("ns3::ConstantRandomVariable[Constant=50]"));
@@ -290,7 +321,7 @@ main(int argc, char *argv[])
 	clientApp9 = client9.Install(csmaNodes1.Get(2));
 	clientApp9.Start(Seconds(2));
 	clientApp9.Stop(Seconds(3));
-	//host  h5--->h8
+		//host  n5--->n8
 	ApplicationContainer clientApp10;
 	OnOffHelper client10("ns3::TcpSocketFactory", InetSocketAddress(csmaInterfaces4.GetAddress(2), 5000));
 	client10.SetAttribute("OnTime", StringValue("ns3::ConstantRandomVariable[Constant=50]"));
@@ -312,10 +343,9 @@ main(int argc, char *argv[])
 	csma4.EnablePcap("dc2", csmaNodes4.Get(1)->GetId(), 0, false);// csmaDevices4.Get (1));
 	csma4.EnablePcap("dc2", csmaNodes4.Get(2)->GetId(), 0, false);// csmaDevices4.Get (2));
 
+	
 
-	  //AnimationInterface anim("dc.xml"); 
-
-
+	//AnimationInterface anim("dc.xml"); //turn off the xml(for NetAnim) output
 
 
 
